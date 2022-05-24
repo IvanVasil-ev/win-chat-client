022c1ce5e438d16617404235f96b0dfa594a92fd
Ivan Vasilev <i.vasilev@dunice.net>
2022-05-24 12:17:33 +0300

    Style chat page

--- src/pages/Main/Chats/Chat/Chat.tsx
+++ src/pages/Main/Chats/Chat/Chat.tsx
@@ -1,17 +1,93 @@
+import { useEffect, useRef, useState } from 'react';
+import cx from 'clsx';
+
 import { Button, Input } from '../../../../components';
 import styles from './Chat.module.scss';
 
+const currentChat = [
+  {
+    id: 1,
+    title: 'Ivan Vasilev',
+    message: 'Hello!',
+    isYourMessage: true,
+    datetime: '08:12',
+  },
+  {
+    id: 2,
+    title: 'Ivan Ivanov',
+    message: 'Hello!',
+    isYourMessage: false,
+    datetime: '10:29',
+  },
+  {
+    id: 3,
+    title: 'Ivan Vasilev',
+    message: 'How are you?',
+    isYourMessage: true,
+    datetime: '12:10',
+  },
+  {
+    id: 4,
+    title: 'Ivan Ivanov',
+    message: 'good!',
+    isYourMessage: false,
+    datetime: '12:10',
+  },
+  {
+    id: 5,
+    title: 'Ivan Ivanov',
+    message: 'Are you?',
+    isYourMessage: false,
+    datetime: '12:10',
+  },
+].reverse();
+
 export const Chat = () => {
+  const [message, setMessage] = useState('');
+  const chatRef = useRef<null | HTMLDivElement>(null);
+  const scrollToBottom = () => {
+    chatRef.current !== null && chatRef.current.scrollIntoView(false);
+  };
+
+  const onMessagesend = () => {
+    setMessage('');
+    scrollToBottom();
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
+
   return (
     <div className={styles.chatContainer}>
-      <div className={styles.chatWrapper}>
-        123
+      <div className={styles.chat}>
+        <div className={styles.chatHistory}>
+          {currentChat.map(({
+            id, title, message, isYourMessage, datetime,
+          }) => (
+            <div
+              key={`${id}_${message}`}
+              className={cx(styles.chatMessageWrapper, isYourMessage && styles.own)}
+            >
+              <div className={styles.chatMessageTitle}>
+                {title}
+              </div>
+              <div className={styles.chatMessage}>
+                {message}
+              </div>
+              <div className={styles.messageInfo}>
+                {datetime}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div ref={chatRef} />
       </div>
       <div className={styles.chatInputWrapper}>
         <div className={styles.chatInput}>
-          <Input value="" noError />
+          <Input value={message} onChange={setMessage} noError />
         </div>
-        <Button style={styles.button}>
+        <Button style={styles.button} onClick={onMessagesend}>
           Send
         </Button>
       </div>
--- src/pages/Main/Chats/ChatList/ChatsList.tsx
+++ src/pages/Main/Chats/ChatList/ChatsList.tsx
@@ -1,11 +1,176 @@
+import cx from 'clsx';
+
 import { Divider } from '../../../../components';
+import thumbnail from '../../../../assets/profile-thumbnail.jpeg';
 import styles from './ChatList.module.scss';
 
+const chats = [
+  {
+    id: 1,
+    title: 'Ivan Ivanov',
+    status: 'NEW',
+    type: 'MESSAGES',
+    lastMessage: 'Hello!',
+    isYourMessage: false,
+    image: thumbnail,
+    datetime: '12:10',
+  },
+  {
+    id: 2,
+    title: 'Alex Curszy',
+    status: 'UNREAD',
+    type: 'MESSAGES',
+    lastMessage: 'How are you? 123123123123213 123123123123123123',
+    isYourMessage: true,
+    image: thumbnail,
+    datetime: '09:22',
+  },
+  {
+    id: 3,
+    title: 'Friends',
+    status: 'READ',
+    type: 'GROUPS',
+    lastMessage: 'Hello!',
+    isYourMessage: false,
+    image: thumbnail,
+    datetime: '09:20',
+  },
+  {
+    id: 4,
+    title: 'Friends',
+    status: 'READ',
+    type: 'GROUPS',
+    lastMessage: 'Hello!',
+    isYourMessage: false,
+    image: thumbnail,
+    datetime: '09:20',
+  },
+  {
+    id: 5,
+    title: 'Friends',
+    status: 'READ',
+    type: 'GROUPS',
+    lastMessage: 'Hello!',
+    isYourMessage: false,
+    image: thumbnail,
+    datetime: '09:20',
+  },
+  {
+    id: 6,
+    title: 'Stan Smith',
+    status: 'READ',
+    type: 'MESSAGES',
+    lastMessage: 'Hello!',
+    isYourMessage: false,
+    image: thumbnail,
+    datetime: '09:20',
+  },
+  {
+    id: 7,
+    title: 'Alex Alex',
+    status: 'READ',
+    type: 'MESSAGES',
+    lastMessage: 'Hello!',
+    isYourMessage: false,
+    image: thumbnail,
+    datetime: '09:20',
+  },
+  {
+    id: 8,
+    title: 'Only me!',
+    status: 'READ',
+    type: 'MESSAGES',
+    lastMessage: 'Hello!',
+    isYourMessage: false,
+    image: thumbnail,
+    datetime: '09:20',
+  },
+  {
+    id: 9,
+    title: 'Friends',
+    status: 'READ',
+    type: 'GROUPS',
+    lastMessage: 'Hello!',
+    isYourMessage: false,
+    image: thumbnail,
+    datetime: '09:20',
+  },
+];
+
 export const ChatList = () => {
+  const statusStyle = (status: string) => {
+    switch (status) {
+      case 'NEW':
+        return styles.newStatus;
+      case 'UNREAD':
+        return styles.unreadStatus;
+      default:
+        return null;
+    }
+  };
+
+  const onChatClick = () => {};
+
   return (
     <div className={styles.chatListContainer}>
       <Divider text="Messages" />
+      <div className={styles.chatWrapper}>
+        {chats.filter(({ type }) => type === 'MESSAGES').map(({
+          id, title, status, lastMessage, isYourMessage, image, datetime,
+        }) => (
+          <div
+            key={`${id}_${title}`}
+            className={styles.chatContainer}
+            onClick={onChatClick}
+            aria-hidden
+          >
+            <img src={image} alt="" className={styles.chatImage} />
+            <div className={styles.chatContent}>
+              <span className={styles.chatTitle}>
+                {title}
+              </span>
+              <span className={styles.chatLastMessage}>
+                {`${isYourMessage ? 'You: ' : ''}${lastMessage}`}
+              </span>
+            </div>
+            <div className={styles.chatInfo}>
+              <span className={styles.chatDatetime}>
+                {datetime}
+              </span>
+              <div className={cx(styles.chatStatus, statusStyle(status))} />
+            </div>
+          </div>
+        ))}
+      </div>
       <Divider text="Groups" />
+      <div className={styles.chatWrapper}>
+        {chats.filter(({ type }) => type === 'GROUPS').map(({
+          id, title, status, lastMessage, isYourMessage, image, datetime,
+        }) => (
+          <div
+            key={`${id}_${title}`}
+            className={styles.chatContainer}
+            onClick={onChatClick}
+            aria-hidden
+          >
+            <img src={image} alt="" className={styles.chatImage} />
+            <div className={styles.chatContent}>
+              <span className={styles.chatTitle}>
+                {title}
+              </span>
+              <span className={styles.chatLastMessage}>
+                {`${isYourMessage ? 'You: ' : ''}${lastMessage}`}
+              </span>
+            </div>
+            <div className={styles.chatInfo}>
+              <span className={styles.chatDatetime}>
+                {datetime}
+              </span>
+              <div className={cx(styles.chatStatus, statusStyle(status))} />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
